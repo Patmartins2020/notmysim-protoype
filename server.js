645bc19385bc08d83e250ff2c9d24d0ff3272f9e
Patmartins2020 <paakata@outlook.com>
2025-04-06 19:00:31 +0200

    Fresh connection to GitHub

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files (like HTML, CSS, JS)
+app.use(express.static(__dirname));
+
+// Send start.html as default page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'start.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`NotMySim dashboard running at http://localhost:${PORT}`);
+});
